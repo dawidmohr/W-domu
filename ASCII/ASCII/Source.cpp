b77dcd95b6a599df7b48e9cc9ee737fd96590247
dawidmohr <dawidmm1@gmail.com>
2015-11-24 22:02:30 +0100

    cx

---
+++ ASCII/ASCII/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int podaj;
+	char znak;
+	cout << "podaj";
+	cin >> znak;
+	podaj = (int)znak;
+	cout << "jest to " << podaj << endl;
+	system("pause");
+
+}
