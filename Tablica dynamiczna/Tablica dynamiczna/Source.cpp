1abdee9ac99ccce045ac074a47f558b90924913b
dawidmohr <dawidmm1@gmail.com>
2015-11-26 10:23:24 +0100

    vcv

--- Tablica dynamiczna/Tablica dynamiczna/Source.cpp
+++ Tablica dynamiczna/Tablica dynamiczna/Source.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-	
+	int ** tablica = new int *[3];
 
+	tablica[0] = new int[3];   // wskaŸnik tablica[0] wskazuje na now¹ tablicê
+	tablica[1] = new int[3];   // wskaŸnik tablica[1] wskazuje na now¹ tablicê
+	tablica[2] = new int[3];   // wskaŸnik tablica[2] wskazuje na now¹ tablicê
 
+	tablica[2][2] = 123;
+	cout << tablica[2][2];
 
+	// zwalniamy pamiec
+	delete[] tablica[0];
+	delete[] tablica[1];
+	delete[] tablica[2];
+	delete[] tablica;
 
+	return 0;
 }
